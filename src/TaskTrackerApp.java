249644c8013dc6170b009dbd0beadd2806aadad6
AlcidesC.Chitsumba <alcidesdaconceicao03@gmail.com>
2025-01-11 23:19:32 +0200

    TaskFileManager new feature:  create directory(root and subdir)

--- src/TaskTrackerApp.java
+++ src/TaskTrackerApp.java
@@ -1,8 +1,11 @@
+import filesys.TaskFileManager;
+
+
 public class TaskTrackerApp {
     public static void main(String[] args) {
-        String path = "C:\\Users\\Alcy_DevMode\\Desktop\\Projects\\TaskTracker\\taskTracker\\src\\taskdb\\";
-        FileManager file = new FileManager("NewFileTest2", path);
 
-        file.writeOnFile();
+TaskFileManager file = new TaskFileManager("auth",  "taskdb");
+
+    file.createDirectory();
     }
 }
---
+++ src/filesys/TaskFileManager.java
@@ -0,0 +1,88 @@
+package filesys;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TaskFileManager {
+    private final String fileName;
+    private final String filePath;
+    private final String rootPath;
+
+    public TaskFileManager(String fileName, String filePath) {
+        this.fileName = fileName + ".csv";
+        this.filePath = filePath;
+        this.rootPath = "C:\\Task Tracker\\";
+
+    }
+
+    public void createFile() {
+        File newFile = new File("c");
+        try {
+            if (newFile.createNewFile()) {
+                System.out.println("File Created successfully" + newFile.getName());
+            } else {
+                System.out.println("Can't create the file!");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Something wrong creating the file");
+        }
+    }
+
+
+    //Create a new directory if it does'nt exists;
+    public void createDirectory() {
+        File newSubDir = new File(rootPath + filePath);
+        ;
+        File rootDir = new File(this.rootPath);
+
+        //Create  app root directories if they don't  exists,
+        if (!(newSubDir.exists() && rootDir.exists())) {
+            if (rootDir.exists()) {
+
+                if (!(newSubDir.exists())) {
+                    if (newSubDir.mkdir()) {
+                        System.out.println("Subdir created");
+                    } else {
+                        System.out.println("Subdir not created");
+                    }
+                } else {
+                    System.out.println("Subdir already created");
+                }
+            } else {
+                if (rootDir.mkdir()) {
+                    System.out.println("Root dir created");
+                } else {
+                    System.out.println("root dir not created");
+                }
+                if (!(newSubDir.exists())) {
+                    if (newSubDir.mkdir()) {
+                        System.out.println("Subdir created");
+                    } else {
+                        System.out.println("Subdir not created");
+                    }
+                } else {
+                    System.out.println("Drectory already created");
+                }
+            }
+        } else  {
+            System.out.println("They already exists!");
+        }
+
+    }
+
+
+    public void writeANewLine() {
+
+        try {
+            FileWriter fWriter = new FileWriter(filePath + "\\" + fileName);
+            fWriter.append("Hello");
+            fWriter.close();
+        } catch (IOException e) {
+            System.out.println("Something is wrong!");
+        }
+    }
+}
+
+
