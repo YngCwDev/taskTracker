86aa435c77242d8d61f602d765992518f6655935
AlcidesC.Chitsumba <alcidesdaconceicao03@gmail.com>
2025-01-13 23:12:18 +0200

    Checkpoint

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,9 +1,29 @@
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class TaskManager {
     private String taskId;
-    private  String description;
-    private  String status;
-    private  String createdAt;
-    private  String updatedAt;
+    private String description;
+    private String status;
+    private String createdAt;
+    private String updatedAt;
+
+    public TaskManager(String description, String status) {
+        this.taskId = "xxxx";
+        this.description = description;
+        this.status = status;
+        this.createdAt = getCreatedAt();
+        this.updatedAt = getUpdatedAt();
+    }
 
 
+    public String getCreatedAt() {
+        Date date = new Date();
+        Timestamp time = new Timestamp(date.getTime());
+        return String.valueOf(time);
+    }
+
+    public String getUpdatedAt() {
+        return "";
+    }
 }
--- src/TaskTrackerApp.java
+++ src/TaskTrackerApp.java
@@ -5,12 +5,8 @@ import filesys.TaskFileManager;
 public class TaskTrackerApp {
     public static void main(String[] args) {
 
-        TaskFileManager file = new TaskFileManager("auth", "taskdb");
+        TaskManager task = new TaskManager("AnyThing", "TODO");
+        System.out.println(task.getCreatedAt());
 
-        User user = new User("YngCidw", "1234");
-        User user2 = new User("Alcy", "8484");
-        System.out.println(user.userExist());
-        user2.login();
     }
-
 }
